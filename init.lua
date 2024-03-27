4fb720d9d84aae588471a3b642c9a8c03da73a5d
Adam Bechtold <adamcbechtold@gmail.com>
2024-03-26 20:59:33 -0400

    Add buffer and command pickers to telescope

--- init.lua
+++ init.lua
@@ -36,7 +36,8 @@ require("lazy").setup(plugins, opts)
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<Leader> ', builtin.find_files, {})
 vim.keymap.set('n', '<Leader>g', builtin.live_grep, {})
-
+vim.keymap.set('n', '<Leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<Leader>fc', builtin.commands, {})
 
 
 
