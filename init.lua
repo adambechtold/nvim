74e4164d44bc1d0a95372905bd9490f50bd9c9f6
Adam Bechtold <adamcbechtold@gmail.com>
2024-03-26 21:31:11 -0400

    Add which-key

--- init.lua
+++ init.lua
@@ -41,6 +41,19 @@ local plugins = {{
         "<cmd>LazyGit<cr>",
         desc = "LazyGit"
     }}
+}, {
+    -- https://github.com/folke/which-key.nvim
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 300
+    end,
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+    }
 }}
 local opts = {}
 
