b17e3761895e14884deb1e7637760ad40b8d9091
Adam Bechtold <adamcbechtold@gmail.com>
2024-03-29 14:14:11 -0400

    Add lsps for python and javascript formatting

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -4,7 +4,14 @@ return {
     local null_ls = require("null-ls")
     null_ls.setup({
       sources = {
+        -- install in the formatting section of mason
         null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.isort,
+
+        -- install eslint_d with mason
+        -- null_ls.builtins.diagnostics.eslint_d,
       },
     })
     vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format, {})
