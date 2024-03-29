3b9a4a05a9a3ea3a810d4029c90b31000f1453c6
Adam Bechtold <adamcbechtold@gmail.com>
2024-03-29 13:06:38 -0400

    Organize whichkey into a separate file

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,17 +1,4 @@
-return {{
-  -- https://github.com/folke/which-key.nvim
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-  end,
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+return {{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
   {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
