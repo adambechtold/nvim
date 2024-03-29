0a393067259b78a0191ec50ee8f5a89fd0db7ce6
Adam Bechtold <adamcbechtold@gmail.com>
2024-03-29 12:50:29 -0400

    Organize plugins into separate file and separate colorscheme

--- init.lua
+++ init.lua
@@ -21,54 +21,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {{
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000
-}, {
-  'nvim-telescope/telescope.nvim',
-  tag = '0.1.6',
-  dependencies = {'nvim-lua/plenary.nvim'}
-}, {
-  "kdheepak/lazygit.nvim",
-  cmd = {"LazyGit", "LazyGitConfig", "LazyGitCurrentFile", "LazyGitFilter", "LazyGitFilterCurrentFile"},
-  -- optional for floating window border decoration
-  dependencies = {"nvim-lua/plenary.nvim"},
-  -- setting the keybinding for LazyGit with 'keys' is recommended in
-  -- order to load the plugin when the command is run for the first time
-  keys = {{
-    "<leader>lg",
-    "<cmd>LazyGit<cr>",
-    desc = "LazyGit"
-  }}
-}, {
-  -- https://github.com/folke/which-key.nvim
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-  end,
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    }
-  }
-}}
-local opts = {}
-
-require("lazy").setup(plugins, opts)
+require("lazy").setup("plugins")
 
 -- Configure Telescope
 local builtin = require('telescope.builtin')
@@ -77,9 +30,6 @@ vim.keymap.set('n', '<Leader>g', builtin.live_grep, {})
 vim.keymap.set('n', '<Leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<Leader>fc', builtin.commands, {})
 
--- Configure Catppuccin Theme 
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
 
 -- Configure TreeSitter
 local tree_sitter_config = require("nvim-treesitter.configs")
---
+++ lua/plugins.lua
@@ -0,0 +1,41 @@
+return {{
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.6',
+  dependencies = {'nvim-lua/plenary.nvim'}
+}, {
+  "kdheepak/lazygit.nvim",
+  cmd = {"LazyGit", "LazyGitConfig", "LazyGitCurrentFile", "LazyGitFilter", "LazyGitFilterCurrentFile"},
+  -- optional for floating window border decoration
+  dependencies = {"nvim-lua/plenary.nvim"},
+  -- setting the keybinding for LazyGit with 'keys' is recommended in
+  -- order to load the plugin when the command is run for the first time
+  keys = {{
+    "<leader>lg",
+    "<cmd>LazyGit<cr>",
+    desc = "LazyGit"
+  }}
+}, {
+  -- https://github.com/folke/which-key.nvim
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  init = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 300
+  end,
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+  }
+}}
