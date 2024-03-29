e6ef24a12f448f181f05e4bc115fd77345b97c79
Adam Bechtold <adamcbechtold@gmail.com>
2024-03-29 12:40:18 -0400

    Install and configure neotree

--- init.lua
+++ init.lua
@@ -22,39 +22,49 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {{
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000
 }, {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.6',
-    dependencies = {'nvim-lua/plenary.nvim'}
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.6',
+  dependencies = {'nvim-lua/plenary.nvim'}
 }, {
-    "kdheepak/lazygit.nvim",
-    cmd = {"LazyGit", "LazyGitConfig", "LazyGitCurrentFile", "LazyGitFilter", "LazyGitFilterCurrentFile"},
-    -- optional for floating window border decoration
-    dependencies = {"nvim-lua/plenary.nvim"},
-    -- setting the keybinding for LazyGit with 'keys' is recommended in
-    -- order to load the plugin when the command is run for the first time
-    keys = {{
-        "<leader>lg",
-        "<cmd>LazyGit<cr>",
-        desc = "LazyGit"
-    }}
+  "kdheepak/lazygit.nvim",
+  cmd = {"LazyGit", "LazyGitConfig", "LazyGitCurrentFile", "LazyGitFilter", "LazyGitFilterCurrentFile"},
+  -- optional for floating window border decoration
+  dependencies = {"nvim-lua/plenary.nvim"},
+  -- setting the keybinding for LazyGit with 'keys' is recommended in
+  -- order to load the plugin when the command is run for the first time
+  keys = {{
+    "<leader>lg",
+    "<cmd>LazyGit<cr>",
+    desc = "LazyGit"
+  }}
 }, {
-    -- https://github.com/folke/which-key.nvim
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    init = function()
-        vim.o.timeout = true
-        vim.o.timeoutlen = 300
-    end,
-    opts = {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-    },
-    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+  -- https://github.com/folke/which-key.nvim
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  init = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 300
+  end,
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+  }
 }}
 local opts = {}
 
@@ -78,6 +88,9 @@ tree_sitter_config.setup({
   highlight = { enabled = true },
   indent = { enabled = true }
 })
+
+-- Configure NeoTree
+vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal right<CR>', {})
 
 --[[
 Plugins of Interest:
