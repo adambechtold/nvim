d1e81b1fa05195c7e93580009e4eb3dad3313849
Adam Bechtold <adamcbechtold@gmail.com>
2024-03-29 13:55:36 -0400

    Install typescript lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -10,7 +10,7 @@ return {
     config = function()
       require("mason-lspconfig").setup({
         -- Available LSPs - https://github.com/williamboman/mason-lspconfig.nvim?tab=readme-ov-file#available-lsp-servers
-        ensure_installed = { "lua_ls" }
+        ensure_installed = { "lua_ls", "tsserver" }
       })
     end
   },
@@ -19,6 +19,8 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
+      lspconfig.tsserver.setup({})
+
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
