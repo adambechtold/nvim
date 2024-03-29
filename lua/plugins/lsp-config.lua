ee562f646f397e532631118d200b6519b3705dac
Adam Bechtold <adamcbechtold@gmail.com>
2024-03-29 13:52:32 -0400

    Add lsp configuration and some key bindings for lua

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,13 +1,13 @@
 return {
   {
     "williamboman/mason.nvim",
-    config = function() 
+    config = function()
       require("mason").setup()
     end
-  }, 
+  },
   {
     "williamboman/mason-lspconfig.nvim",
-    config = function() 
+    config = function()
       require("mason-lspconfig").setup({
         -- Available LSPs - https://github.com/williamboman/mason-lspconfig.nvim?tab=readme-ov-file#available-lsp-servers
         ensure_installed = { "lua_ls" }
@@ -16,9 +16,12 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
-    config = function() 
+    config = function()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
     end
   }
 }
