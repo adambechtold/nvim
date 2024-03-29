2cc123637c770de3a23713284a7a51a3c2bf47ef
Adam Bechtold <adamcbechtold@gmail.com>
2024-03-29 18:44:42 -0400

    Enable sticky headers with treesitter context plugin

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,17 @@
 return {
-  "nvim-treesitter/nvim-treesitter", 
-  build = ":TSUpdate", 
-  config = function()
-
-    local tree_sitter_config = require("nvim-treesitter.configs")
-    tree_sitter_config.setup({
-      auto_install = true,
-      highlight = { enabled = true },
-      indent = { enabled = true }
-    })
-  end
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local tree_sitter_config = require("nvim-treesitter.configs")
+      tree_sitter_config.setup({
+        auto_install = true,
+        highlight = { enabled = true },
+        indent = { enabled = true },
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+  },
 }
-
