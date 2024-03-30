8571d8dfe139e6c79551e1d85dc3f52194e4a895
Adam Bechtold <adamcbechtold@gmail.com>
2024-03-30 19:32:31 -0400

    Apply style guide to indents file

--- lua/plugins/indents-config.lua
+++ lua/plugins/indents-config.lua
@@ -1,39 +1,40 @@
 return {
-	{
-		"echasnovski/mini.indentscope",
+  {
+    "echasnovski/mini.indentscope",
+    enabled = true,
+    config = function()
+      require("mini.indentscope").setup({
+        draw = {
+          delay = 25,           -- default value
+          ---@diagnostic disable-next-line: unused-local
+          animation = function(s, n) -- return the time between steps (s is the next step, n is the total number of steps)
+            if n < 30 then
+              return 20
+            end
+            return 30 / (s / 2)
+          end,
+        },
+      })
+    end,
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
     enabled = false,
-		config = function()
-			require("mini.indentscope").setup({
-				draw = {
-					delay = 25, -- default value
-					---@diagnostic disable-next-line: unused-local
-					animation = function(s, n) -- return the time between steps (s is the next step, n is the total number of steps)
-						if n < 30 then
-							return 20
-						end
-						return 30 / (s / 2)
-					end,
-				},
-			})
-		end,
-	},
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		config = function()
-			local highlight = {
-				"CursorColumn",
-				"Whitespace",
-			}
+    config = function()
+      local highlight = {
+        "CursorColumn",
+        "Whitespace",
+      }
 
-			require("ibl").setup({
+      require("ibl").setup({
 
-				indent = { highlight = highlight, char = "|" },
-				whitespace = {
-					highlight = highlight,
-					remove_blankline_trail = true,
-				},
-				scope = { enabled = true }, -- enabled by default
-			})
-		end,
-	},
+        indent = { highlight = highlight, char = "|" },
+        whitespace = {
+          highlight = highlight,
+          remove_blankline_trail = true,
+        },
+        scope = { enabled = true }, -- enabled by default
+      })
+    end,
+  },
 }
