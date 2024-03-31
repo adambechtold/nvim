5fd96b6f9bcd32a0f84012dfa521d6cd0a9d34ce
Adam Bechtold <adamcbechtold@gmail.com>
2024-03-30 22:14:30 -0400

    Increase speed of the indent marker

--- lua/plugins/indents-config.lua
+++ lua/plugins/indents-config.lua
@@ -9,7 +9,7 @@ return {
           ---@diagnostic disable-next-line: unused-local
           animation = function(s, n) -- return the time between steps (s is the next step, n is the total number of steps)
             if n < 30 then
-              return 20
+              return 15 
             end
             return 30 / (s / 2)
           end,
