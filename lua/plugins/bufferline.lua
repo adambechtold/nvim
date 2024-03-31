e3f1a810481a6bb286684934febed9e617e2c3e2
Adam Bechtold <adamcbechtold@gmail.com>
2024-03-30 22:49:09 -0400

    Configure bufferline

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    config = function()
+      -- vim.opt.termguicolors = true
+      -- type :h bufferline-configuration for more info
+      require("bufferline").setup({
+        options = {
+          separator_style = "slant",
+        },
+      })
+    end,
+  },
+}
