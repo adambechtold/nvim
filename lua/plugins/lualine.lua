3138d92f40c51a89478ed0d651acd4a890e068c6
Adam Bechtold <adamcbechtold@gmail.com>
2024-03-31 12:08:33 -0400

    Update lualine when we change the theme

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,6 +5,10 @@ return {
 			options = {
 				theme = "dracula", -- works great with catppuccin
 				-- theme = "everforest",
+				-- every time we change themes in whichkey, we update the theme of lua line
+				-- Right now, it calls setup again, which is not idea, because we might have
+				-- other options besides theme that we want to keeping
+				-- We can solve this if/when we run into that problem
 			},
 		})
 	end,
--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,62 +1,87 @@
 return {
-  -- https://github.com/folke/which-key.nvim
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-  end,
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
-  config = function()
-    local wk = require("which-key")
-    wk.register({
-      ["]r"] = { "<cmd>lua require('illuminate').goto_next_reference{wrap=true}<CR>", "Next Reference" },
-      ["[r"] = { "<cmd>lua require('illuminate').goto_prev_reference{wrap=true}<CR>", "Previous Reference" },
+	-- https://github.com/folke/which-key.nvim
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	init = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 300
+	end,
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+	},
+	config = function()
+		local lualineCmd = function(t)
+			return string.format("<cmd>lua require('lualine').setup({options = {theme = '%s'}})<CR>", t)
+		end
 
-      -- Move between buffers
-      ["]b"] = { ":bp<CR>", "Previous Buffer" },
-      ["[b"] = { ":bn<CR>", "Next Buffer" },
+		local wk = require("which-key")
+		wk.register({
+			["]r"] = { "<cmd>lua require('illuminate').goto_next_reference{wrap=true}<CR>", "Next Reference" },
+			["[r"] = { "<cmd>lua require('illuminate').goto_prev_reference{wrap=true}<CR>", "Previous Reference" },
 
-      -- Telescope
-      ["<leader> "] = { "<cmd>Telescope find_files<CR>", "Find Files" },
-      ["<leader>fg"] = { "<cmd>Telescope live_grep<CR>", "Live Grep" },
-      ["<leader>fb"] = { "<cmd>Telescope buffers<CR>", "Buffers" },
-      ["<leader>fc"] = { "<cmd>Telescope commands<CR>", "Commands" },
-      ["<leader>fh"] = { "<cmd>Telescope help_tags<CR>", "Help Tags" },
-      ["<leader>fs"] = { "<cmd>Telescope search_history<CR>", "Search History" },
+			-- Move between buffers
+			["]b"] = { ":bp<CR>", "Previous Buffer" },
+			["[b"] = { ":bn<CR>", "Next Buffer" },
 
-      -- Display Settings
-      ["<leader>s"] = "Settings",
-      ["<leader>sd"] = { "<cmd>set background=dark<CR>", "Dark Background" },
-      ["<leader>sl"] = { "<cmd>set background=light<CR>", "Light Background" },
-      -- Themes
-      ["<leader>sc"] = "Colorschemes",
+			-- Telescope
+			["<leader> "] = { "<cmd>Telescope find_files<CR>", "Find Files" },
+			["<leader>fg"] = { "<cmd>Telescope live_grep<CR>", "Live Grep" },
+			["<leader>fb"] = { "<cmd>Telescope buffers<CR>", "Buffers" },
+			["<leader>fc"] = { "<cmd>Telescope commands<CR>", "Commands" },
+			["<leader>fh"] = { "<cmd>Telescope help_tags<CR>", "Help Tags" },
+			["<leader>fs"] = { "<cmd>Telescope search_history<CR>", "Search History" },
 
-      ["<leader>sce"] = { "<cmd>colorscheme everforest<CR>", "Everforest" },
-      ["<leader>scc"] = { "<cmd>colorscheme catppuccin<CR>", "Catppuccin" },
+			-- Display Settings
+			["<leader>s"] = "Settings",
+			["<leader>sd"] = { "<cmd>set background=dark<CR>", "Dark Background" },
+			["<leader>sl"] = { "<cmd>set background=light<CR>", "Light Background" },
+			-- Themes
+			["<leader>sc"] = "Colorschemes",
 
-      ["<leader>sct"] = "Tokyonight (multiple options)",
-      ["<leader>sct<CR>"] = { "<cmd>colorscheme tokyonight-storm<CR>", "Tokyonight Storm (darker)" },
-      ["<leader>scts"] = { "<cmd>colorscheme tokyonight-storm<CR>", "Tokyonight Storm (darker)" },
-      ["<leader>sctn"] = { "<cmd>colorscheme tokyonight-night<CR>", "Tokyonight Night (darkest)" },
-      ["<leader>sctm"] = { "<cmd>colorscheme tokyonight-moon<CR>", "Tokyonight Moon (dark-ish)" },
-      ["<leader>sctd"] = { "<cmd>colorscheme tokyonight-day<CR>", "Tokyonight (light)" },
+			["<leader>sce"] = {
+				string.format("<cmd>lua vim.cmd [[colorscheme everforest]]<CR>%s", lualineCmd("everforest")),
+				"Everforest",
+			},
+			["<leader>scc"] = {
+				string.format("<cmd>colorscheme catppuccin<CR>%s", lualineCmd("darcula")),
+				"Catppuccin",
+			},
 
-      -- Git Signs
-      ["<leader>g"] = "Git",
-      ["<leader>gs"] = { "<cmd>lua require('gitsigns').stage_hunk()<CR>", "Stage Hunk" },
-      ["<leader>gu"] = { "<cmd>lua require('gitsigns').undo_stage_hunk()<CR>", "Undo Stage Hunk" },
-      ["<leader>gr"] = { "<cmd>lua require('gitsigns').reset_hunk()<CR>", "Reset Hunk" },
-      ["<leader>gR"] = { "<cmd>lua require('gitsigns').reset_buffer()<CR>", "Reset Buffer" },
-      ["<leader>gp"] = { "<cmd>lua require('gitsigns').preview_hunk()<CR>", "Preview Hunk" },
-      ["<leader>gb"] = { "<cmd>lua require('gitsigns').blame_line()<CR>", "Blame Line" },
-      ["]h"] = { "<cmd>lua require('gitsigns').next_hunk()<CR>", "Next Hunk" },
-      ["[h"] = { "<cmd>lua require('gitsigns').prev_hunk()<CR>", "Previous Hunk" },
-      ["<leader>gl"] = { "<cmd>LazyGit<CR>", "LazyGit" },
-    })
-  end,
+			["<leader>sct"] = "Tokyonight (multiple options)",
+			["<leader>sct<CR>"] = {
+				string.format("<cmd>colorscheme tokyonight-storm<CR>%s", lualineCmd("darcula")),
+				"Tokyonight Storm (darker)",
+			},
+			["<leader>scts"] = {
+				string.format("<cmd>colorscheme tokyonight-storm<CR>%s", lualineCmd("darcula")),
+				"Tokyonight Storm (darker)",
+			},
+			["<leader>sctn"] = {
+				string.format("<cmd>colorscheme tokyonight-night<CR>%s", lualineCmd("darcula")),
+				"Tokyonight Night (darkest)",
+			},
+			["<leader>sctm"] = {
+				string.format("<cmd>colorscheme tokyonight-moon<CR>%s", lualineCmd("darcula")),
+				"Tokyonight Moon (dark-ish)",
+			},
+			["<leader>sctd"] = {
+				string.format("<cmd>colorscheme tokyonight-day<CR>%s", lualineCmd("darcula")),
+				"Tokyonight (light)",
+			},
+
+			-- Git Signs
+			["<leader>g"] = "Git",
+			["<leader>gs"] = { "<cmd>lua require('gitsigns').stage_hunk()<CR>", "Stage Hunk" },
+			["<leader>gu"] = { "<cmd>lua require('gitsigns').undo_stage_hunk()<CR>", "Undo Stage Hunk" },
+			["<leader>gr"] = { "<cmd>lua require('gitsigns').reset_hunk()<CR>", "Reset Hunk" },
+			["<leader>gR"] = { "<cmd>lua require('gitsigns').reset_buffer()<CR>", "Reset Buffer" },
+			["<leader>gp"] = { "<cmd>lua require('gitsigns').preview_hunk()<CR>", "Preview Hunk" },
+			["<leader>gb"] = { "<cmd>lua require('gitsigns').blame_line()<CR>", "Blame Line" },
+			["]h"] = { "<cmd>lua require('gitsigns').next_hunk()<CR>", "Next Hunk" },
+			["[h"] = { "<cmd>lua require('gitsigns').prev_hunk()<CR>", "Previous Hunk" },
+			["<leader>gl"] = { "<cmd>LazyGit<CR>", "LazyGit" },
+		})
+	end,
 }
