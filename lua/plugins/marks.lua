6f115c3229ff7054e423af25e5b35e8aa43e0a29
Adam Bechtold <adamcbechtold@gmail.com>
2024-03-31 20:24:51 -0400

    Configure marks

---
+++ lua/plugins/marks.lua
@@ -0,0 +1,18 @@
+return {
+  "kshenoy/vim-signature",
+  enabled = true,
+}
+
+--[[
+Create Mark (m + character)
+Delete Mark (dm + character)
+Jump to Mark Line (' + character)
+Jump to Mark Col (` + character`)
+
+Jump to next mark in file (]')
+Jump to prev mark in file ([')
+Replace ' with ` to cycle between col marks
+
+
+--
+--]]
