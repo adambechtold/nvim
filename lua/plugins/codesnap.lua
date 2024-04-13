658f2c11f4e20db144761b0599dc714d66c21763
Adam Bechtold <adamcbechtold@gmail.com>
2024-04-13 09:53:15 -0400

    Adjust codesnap style - no watermark, better font

--- lua/plugins/codesnap.lua
+++ lua/plugins/codesnap.lua
@@ -6,9 +6,10 @@ return {
 			require("codesnap").setup({
 				has_breadcrumbs = true,
 				bg_theme = "sea",
-				watermark = "vibeiq",
+				watermark = "",
 				mac_window_bar = true,
 				save_path = "~/Downloads",
+        code_font_family = "DejaVuSansM Nerd Font"
 			})
 		end,
 	},
