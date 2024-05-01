acd51b4f4f9c7f8e320bba9a0a673f636f917810
Adam Bechtold <adamcbechtold@gmail.com>
2024-04-30 20:00:06 -0400

    Incrase size of aerial float panel and remove ctrl-a for the side-panel view

--- lua/plugins/aerial.lua
+++ lua/plugins/aerial.lua
@@ -9,12 +9,13 @@ return {
 	config = function()
 		require("aerial").setup({
 			layout = {
-				default_direction = "left",
+				default_direction = "right",
 				placement = "window",
+        min_width = 30,
+        max_width = 150,
 			},
 			highlight_on_hover = true,
 			highlight_on_jump = 500, -- when jumping to a symbol, highlight it for this many ms
 		})
-		vim.keymap.set("n", "<c-a>", "<cmd>AerialToggle<CR>")
 	end,
 }
