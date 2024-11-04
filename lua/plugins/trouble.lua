b56201da572cef79d6da94006ec9124a7aed93bf
Adam Bechtold <adamcbechtold@gmail.com>
2024-11-04 16:04:55 -0500

    Update whichkey to reflect new format of table

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,23 +1,23 @@
 return {
-  "folke/trouble.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
-  config = function()
-    require("which-key").register({
-      ["<leader>x"] = "Trouble",
-      ["<leader>xd"] = { "<cmd>TroubleToggle lsp_definitions<CR>", "LSP Definitions" },
-      ["<leader>xi"] = { "<cmd>TroubleToggle lsp_implementations<CR>", "LSP Implementations" },
-      ["<leader>xt"] = { "<cmd>TroubleToggle lsp_type_definitions<CR>", "LSP Type Definitions" },
-      ["<leader>xr"] = { "<cmd>TroubleToggle lsp_references<CR>", "LSP References" },
-      ["<leader>xz"] = { "<cmd>TroubleToggle lsp_document_diagnostics<CR>", "LSP Document Diagnostics" },
-      ["<leader>xw"] = { "<cmd>TroubleToggle lsp_workspace_diagnostics<CR>", "LSP Workspace Diagnostics" },
-      ["<leader>xl"] = { "<cmd>TroubleToggle loclist<CR>", "Location List" },
-      ["<leader>xq"] = { "<cmd>TroubleToggle quickfix<CR>", "Quickfix" },
-      ["<leader>xx"] = { "<cmd>TroubleToggle<CR>", "Toggle Trouble Display" },
-    })
-  end,
+	"folke/trouble.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+	},
+	config = function()
+		require("which-key").add({
+			{ "<leader>x", desc = "Trouble" },
+			{ "<leader>xd", "<cmd>TroubleToggle lsp_definitions<CR>", desc = "LSP Definitions" },
+			{ "<leader>xi", "<cmd>TroubleToggle lsp_implementations<CR>", desc = "LSP Implementations" },
+			{ "<leader>xl", "<cmd>TroubleToggle loclist<CR>", desc = "Location List" },
+			{ "<leader>xq", "<cmd>TroubleToggle quickfix<CR>", desc = "Quickfix" },
+			{ "<leader>xr", "<cmd>TroubleToggle lsp_references<CR>", desc = "LSP References" },
+			{ "<leader>xt", "<cmd>TroubleToggle lsp_type_definitions<CR>", desc = "LSP Type Definitions" },
+			{ "<leader>xw", "<cmd>TroubleToggle lsp_workspace_diagnostics<CR>", desc = "LSP Workspace Diagnostics" },
+			{ "<leader>xx", "<cmd>TroubleToggle<CR>", desc = "Toggle Trouble Display" },
+			{ "<leader>xz", "<cmd>TroubleToggle lsp_document_diagnostics<CR>", desc = "LSP Document Diagnostics" },
+		})
+	end,
 }
--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -17,145 +17,105 @@ return {
 		end
 
 		local wk = require("which-key")
-		wk.register({
-
-			-- Illuminate - Highlight other references to the text your cursor is on
-			["]r"] = { "<cmd>lua require('illuminate').goto_next_reference{wrap=true}<CR>", "Next Reference" },
-			["[r"] = { "<cmd>lua require('illuminate').goto_prev_reference{wrap=true}<CR>", "Previous Reference" },
-
-			-- Move between buffers
-			["[b"] = { ":bp<CR>", "Previous Buffer" },
-			["]b"] = { ":bn<CR>", "Next Buffer" },
-
-			-- Telescope
-			["<leader> "] = { "<cmd>Telescope find_files<CR>", "Find Files" },
-			["<leader>fg"] = { "<cmd>Telescope live_grep<CR>", "Live Grep" },
-			["<leader>fb"] = { "<cmd>Telescope buffers<CR>", "Buffers" },
-			["<leader>fc"] = { "<cmd>Telescope commands<CR>", "Commands" },
-			["<leader>fh"] = { "<cmd>Telescope help_tags<CR>", "Help Tags" },
-			["<leader>fs"] = { "<cmd>Telescope search_history<CR>", "Search History" },
-			-- todo-comments
-			["<leader>fo"] = { "<cmd>TodoTelescope<CR>", "Search TODOs" },
-
-			-- Git Signs - Review changes inline
-			["<leader>g"] = "Git",
-			["<leader>gs"] = { "<cmd>lua require('gitsigns').stage_hunk()<CR>", "Stage Hunk" },
-			["<leader>gu"] = { "<cmd>lua require('gitsigns').undo_stage_hunk()<CR>", "Undo Stage Hunk" },
-			["<leader>gr"] = { "<cmd>lua require('gitsigns').reset_hunk()<CR>", "Reset Hunk" },
-			["<leader>gR"] = { "<cmd>lua require('gitsigns').reset_buffer()<CR>", "Reset Buffer" },
-			["<leader>gp"] = { "<cmd>lua require('gitsigns').preview_hunk()<CR>", "Preview Hunk" },
-			["<leader>gb"] = { "<cmd>lua require('gitsigns').blame_line()<CR>", "Blame Line" },
-			["<leader>gt"] = { "<cmd>:Gitsigns toggle_current_line_blame<CR>", "Toggle Git Blame Current Line" },
-			["]h"] = { "<cmd>lua require('gitsigns').next_hunk()<CR>", "Next Hunk" },
-			["[h"] = { "<cmd>lua require('gitsigns').prev_hunk()<CR>", "Previous Hunk" },
-			["<leader>gl"] = { "<cmd>LazyGit<CR>", "LazyGit" },
-
-			-- Terminal - Update - 2024-04-09 - I usually use tmux for terminal
-			--[[
-      ["<leader>t"] = "Terminal",
-      ["<leader>tv"] = { "<cmd>vsplit term://zsh<CR>i", "New Terminal Vertical Split" },
-      ["<leader>th"] = { "<cmd>split<CR><c-w>j <cmd>term<CR>i", "New Terminal Horizontal Split" },
-      ["<leader>tt"] = { "<cmd>tabnew term://zsh<CR>", "New Terminal Tab" },
-      ["<leader>tq"] = { "<cmd>bd!<CR>q<CR>", "Close Current Terminal" },
-      ]]
-			-- Reminder: To escape terminal mode, use <C-\><C-n>
-
-			-- Navigate Buffers
-			["<leader>b"] = "Buffers",
-			["bl"] = { "<cmd>ls<CR>", "List Buffers" },
-			["<leader>bd"] = "Delete Buffer Options",
-			["<leader>bdo"] = { "<cmd>%bd|e#|bd#<CR>", "Delete Other Buffers" },
-			--[[  Explanation
-            %bd - close all buffers
-            | (pipe, to run multiple commands
-            e# - open last editor
-            bd# - delete current buffer]]
-
-			-- Settings
-			["<leader>s"] = "Settings",
-			-- Display Colorscheme / Lightmode & Darkmode
-			["<leader>scd"] = { "<cmd>set background=dark<CR>", "Dark Background" },
-			["<leader>scl"] = { "<cmd>set background=light<CR>", "Light Background" },
-			-- Themes
-			["<leader>sc"] = "Colorschemes",
-			-- Theme / Everforest
-			["<leader>sce"] = {
-				string.format("<cmd>lua vim.cmd [[colorscheme everforest]]<CR>%s", lualineCmd("everforest")),
-				"Everforest",
+		wk.add({
+			{ "<leader> ", "<cmd>Telescope find_files<CR>", desc = "Find Files" },
+			{ "<leader>b", desc = "Buffers" },
+			{ "<leader>bd", desc = "Delete Buffer Options" },
+			{ "<leader>bdo", "<cmd>%bd|e#|bd#<CR>", desc = "Delete Other Buffers" },
+			{ "<leader>fb", "<cmd>Telescope buffers<CR>", desc = "Buffers" },
+			{ "<leader>fc", "<cmd>Telescope commands<CR>", desc = "Commands" },
+			{ "<leader>fg", "<cmd>Telescope live_grep<CR>", desc = "Live Grep" },
+			{ "<leader>fh", "<cmd>Telescope help_tags<CR>", desc = "Help Tags" },
+			{ "<leader>fo", "<cmd>TodoTelescope<CR>", desc = "Search TODOs" },
+			{ "<leader>fs", "<cmd>Telescope search_history<CR>", desc = "Search History" },
+			{ "<leader>g", desc = "Git" },
+			{ "<leader>gR", "<cmd>lua require('gitsigns').reset_buffer()<CR>", desc = "Reset Buffer" },
+			{ "<leader>gb", "<cmd>lua require('gitsigns').blame_line()<CR>", desc = "Blame Line" },
+			{ "<leader>gl", "<cmd>LazyGit<CR>", desc = "LazyGit" },
+			{ "<leader>gp", "<cmd>lua require('gitsigns').preview_hunk()<CR>", desc = "Preview Hunk" },
+			{ "<leader>gr", "<cmd>lua require('gitsigns').reset_hunk()<CR>", desc = "Reset Hunk" },
+			{ "<leader>gs", "<cmd>lua require('gitsigns').stage_hunk()<CR>", desc = "Stage Hunk" },
+			{ "<leader>gt", "<cmd>:Gitsigns toggle_current_line_blame<CR>", desc = "Toggle Git Blame Current Line" },
+			{ "<leader>gu", "<cmd>lua require('gitsigns').undo_stage_hunk()<CR>", desc = "Undo Stage Hunk" },
+			{ "<leader>o", desc = "Oil" },
+			{ "<leader>oo", "<cmd>Oil<CR>", desc = "Oil" },
+			{ "<leader>s", desc = "Settings" },
+			{ "<leader>sc", desc = "Colorschemes" },
+			{
+				"<leader>scc",
+				"<cmd>colorscheme catppuccin<CR><cmd>lua require('lualine').setup({options = {theme = 'darcula'}})<CR>",
+				desc = "Catppuccin",
 			},
-			-- Theme / Catppuccin
-			["<leader>scc"] = {
-				string.format("<cmd>colorscheme catppuccin<CR>%s", lualineCmd("darcula")),
-				"Catppuccin",
+			{ "<leader>scd", "<cmd>set background=dark<CR>", desc = "Dark Background" },
+			{
+				"<leader>sce",
+				"<cmd>lua vim.cmd [[colorscheme everforest]]<CR><cmd>lua require('lualine').setup({options = {theme = 'everforest'}})<CR>",
+				desc = "Everforest",
 			},
-			-- Theme / Tokyonight
-			["<leader>sct"] = "Tokyonight (multiple options)",
-			["<leader>sct<CR>"] = {
-				string.format("<cmd>colorscheme tokyonight-storm<CR>%s", lualineCmd("darcula")),
-				"Tokyonight Storm (darker)",
+			{ "<leader>scl", "<cmd>set background=light<CR>", desc = "Light Background" },
+			{ "<leader>scs", desc = "Solarized" },
+			{
+				"<leader>scsd",
+				"<cmd>set background=dark<CR><cmd>colorscheme solarized<CR><cmd>lua require('lualine').setup({options = {theme = 'solarized_dark'}})<CR>",
+				desc = "Solarized Dark",
 			},
-			["<leader>scts"] = {
-				string.format("<cmd>colorscheme tokyonight-storm<CR>%s", lualineCmd("darcula")),
-				"Tokyonight Storm (darker)",
+			{
+				"<leader>scsl",
+				"<cmd>set background=light<CR><cmd>colorscheme solarized<CR><cmd>lua require('lualine').setup({options = {theme = 'solarized_light'}})<CR>",
+				desc = "Solarized Light",
 			},
-			["<leader>sctn"] = {
-				string.format("<cmd>colorscheme tokyonight-night<CR>%s", lualineCmd("darcula")),
-				"Tokyonight Night (darkest)",
+			{ "<leader>sct", desc = "Tokyonight (multiple options)" },
+			{
+				"<leader>sct<CR>",
+				"<cmd>colorscheme tokyonight-storm<CR><cmd>lua require('lualine').setup({options = {theme = 'darcula'}})<CR>",
+				desc = "Tokyonight Storm (darker)",
 			},
-			["<leader>sctm"] = {
-				string.format("<cmd>colorscheme tokyonight-moon<CR>%s", lualineCmd("darcula")),
-				"Tokyonight Moon (dark-ish)",
+			{
+				"<leader>sctd",
+				"<cmd>colorscheme tokyonight-day<CR><cmd>lua require('lualine').setup({options = {theme = 'darcula'}})<CR>",
+				desc = "Tokyonight (light)",
 			},
-			["<leader>sctd"] = {
-				string.format("<cmd>colorscheme tokyonight-day<CR>%s", lualineCmd("darcula")),
-				"Tokyonight (light)",
+			{
+				"<leader>sctm",
+				"<cmd>colorscheme tokyonight-moon<CR><cmd>lua require('lualine').setup({options = {theme = 'darcula'}})<CR>",
+				desc = "Tokyonight Moon (dark-ish)",
 			},
-			-- Theme / Solarized
-			["<leader>scs"] = "Solarized",
-			["<leader>scsl"] = {
-				string.format(
-					"<cmd>set background=light<CR><cmd>colorscheme solarized<CR>%s",
-					lualineCmd("solarized_light")
-				),
-				"Solarized Light",
+			{
+				"<leader>sctn",
+				"<cmd>colorscheme tokyonight-night<CR><cmd>lua require('lualine').setup({options = {theme = 'darcula'}})<CR>",
+				desc = "Tokyonight Night (darkest)",
 			},
-			["<leader>scsd"] = {
-				string.format(
-					"<cmd>set background=dark<CR><cmd>colorscheme solarized<CR>%s",
-					lualineCmd("solarized_dark")
-				),
-				"Solarized Dark",
+			{
+				"<leader>scts",
+				"<cmd>colorscheme tokyonight-storm<CR><cmd>lua require('lualine').setup({options = {theme = 'darcula'}})<CR>",
+				desc = "Tokyonight Storm (darker)",
 			},
-
-			-- Spelling
-			["<leader>ss"] = "Spelling",
-			["<leader>sst"] = { "<cmd>set spell! spelllang=en_us<CR>", "Toggle Spelling" },
-
-			-- Fold Modes
-			["<leader>sz"] = "Folds",
-			["<leader>szm"] = { "<cmd>set foldmethod=manual<CR>", "Manual" },
-			["<leader>szc"] = { "<cmd>set foldmethod=indent<CR>", "Indent" },
-			["<leader>sza"] = { "<cmd>set foldmethod=syntax<CR>", "Syntax" },
-
-			-- Toggle Copilot
-			["<leader>sece"] = { "<cmd>Copilot enable<CR>", "Enable Copilot" },
-			["<leader>secd"] = { "<cmd>Copilot disable<CR>", "Disable Copilot" },
-
-			-- Editor
-			["<leader>se"] = "Editor",
-			["<leader>sez"] = { "<cmd>Goyo<CR>", "Zen Mode" },
-			["<leader>sev"] = { "<cmd>set number!<CR>", "Toggle Line Numbers" },
-			["<leader>seb"] = { "<cmd>set relativenumber!<CR>", "Toggle Relative Line Numbers" },
-			["<leader>seu"] = { "<cmd>set cursorline!<CR>", "Toggle Cursor Line" },
-			["<leader>sej"] = { "<cmd>set cursorcolumn!<CR>", "Toggle Cursor Column" },
-			["<leader>sef"] = { "<cmd>Format<CR>", "Format" },
-			["<leader>seh"] = { "<cmd>set hlsearch!<CR>", "Toggle Highlight Search" },
-			["<leader>sew"] = { "<cmd>set wrap!<CR>", "Toggle Wrap" },
-			["<leader>ser"] = { "<cmd>set ruler!<CR>", "Toggle Ruler" },
-
-			-- oil
-			["<leader>o"] = "Oil",
-			["<leader>oo"] = { "<cmd>Oil<CR>", "Oil" },
-		})
+			{ "<leader>se", desc = "Editor" },
+			{ "<leader>seb", "<cmd>set relativenumber!<CR>", desc = "Toggle Relative Line Numbers" },
+			{ "<leader>secd", "<cmd>Copilot disable<CR>", desc = "Disable Copilot" },
+			{ "<leader>sece", "<cmd>Copilot enable<CR>", desc = "Enable Copilot" },
+			{ "<leader>sef", "<cmd>Format<CR>", desc = "Format" },
+			{ "<leader>seh", "<cmd>set hlsearch!<CR>", desc = "Toggle Highlight Search" },
+			{ "<leader>sej", "<cmd>set cursorcolumn!<CR>", desc = "Toggle Cursor Column" },
+			{ "<leader>ser", "<cmd>set ruler!<CR>", desc = "Toggle Ruler" },
+			{ "<leader>seu", "<cmd>set cursorline!<CR>", desc = "Toggle Cursor Line" },
+			{ "<leader>sev", "<cmd>set number!<CR>", desc = "Toggle Line Numbers" },
+			{ "<leader>sew", "<cmd>set wrap!<CR>", desc = "Toggle Wrap" },
+			{ "<leader>sez", "<cmd>Goyo<CR>", desc = "Zen Mode" },
+			{ "<leader>ss", desc = "Spelling" },
+			{ "<leader>sst", "<cmd>set spell! spelllang=en_us<CR>", desc = "Toggle Spelling" },
+			{ "<leader>sz", desc = "Folds" },
+			{ "<leader>sza", "<cmd>set foldmethod=syntax<CR>", desc = "Syntax" },
+			{ "<leader>szc", "<cmd>set foldmethod=indent<CR>", desc = "Indent" },
+			{ "<leader>szm", "<cmd>set foldmethod=manual<CR>", desc = "Manual" },
+			{ "[b", ":bp<CR>", desc = "Previous Buffer" },
+			{ "[h", "<cmd>lua require('gitsigns').prev_hunk()<CR>", desc = "Previous Hunk" },
+			{ "[r", "<cmd>lua require('illuminate').goto_prev_reference{wrap=true}<CR>", desc = "Previous Reference" },
+			{ "]b", ":bn<CR>", desc = "Next Buffer" },
+			{ "]h", "<cmd>lua require('gitsigns').next_hunk()<CR>", desc = "Next Hunk" },
+			{ "]r", "<cmd>lua require('illuminate').goto_next_reference{wrap=true}<CR>", desc = "Next Reference" },
+			{ "bl", "<cmd>ls<CR>", desc = "List Buffers" },
+		}
+)
 	end,
 }
