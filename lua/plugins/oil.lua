2c9c66a05540e9f64b864997cf9cec0959645c29
Adam Bechtold <adamcbechtold@gmail.com>
2025-01-27 16:46:31 -0500

    Show hidden files in oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -5,7 +5,11 @@ return {
 		-- Optional dependencies
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
-			require("oil").setup()
+			require("oil").setup({
+				view_options = {
+					show_hidden = true,
+				},
+			})
 		end,
 	},
 }
